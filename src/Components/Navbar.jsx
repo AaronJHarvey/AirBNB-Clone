d1c29f8d31b384215daa672bbc55c5fd0a3f0a8b
Aaron <Harvey.AaronJ@gmail.com>
2022-12-06 22:48:05 -0500

    Builds and styles navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from '../Images/airbnb-logo.png';
+
+export default function Navbar () {
+	return (
+		<div>
+			<nav>
+				<img src={Logo} />
+			</nav>
+		</div>
+	);
+}
