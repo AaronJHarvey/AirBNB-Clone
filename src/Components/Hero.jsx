287c1f4ba1e02116f9dc70a3cb19ae36763fc3ab
Aaron <Harvey.AaronJ@gmail.com>
2022-12-07 00:01:06 -0500

    Styles hero component

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,9 @@ import Grid from '../Images/photo-grid.png';
 export default function Hero () {
 	return (
 		<section>
-			<img src={Grid} />
+			<div className='grid-container'>
+				<img src={Grid} />
+			</div>
 			<h1>Online Experiences</h1>
 			<p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
 		</section>
