fcdce14f93de505f2328ad04bb2854a8d85eb283
Aaron <Harvey.AaronJ@gmail.com>
2022-12-06 23:28:13 -0500

    Adds grid photos to hero component

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ import Grid from '../Images/photo-grid.png';
 export default function Hero () {
 	return (
 		<section>
-			<img />
+			<img src={Grid} />
 			<h1>Online Experiences</h1>
 			<p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
 		</section>
