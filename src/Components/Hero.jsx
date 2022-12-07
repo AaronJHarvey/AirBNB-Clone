ed5890ff0f04a6ef9dbd302d5b7f8a063b0795d8
Aaron <Harvey.AaronJ@gmail.com>
2022-12-06 23:26:03 -0500

    Creates Hero component

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Grid from '../Images/photo-grid.png';
+
+export default function Hero () {
+	return (
+		<section>
+			<img />
+			<h1>Online Experiences</h1>
+			<p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+		</section>
+	);
+}
